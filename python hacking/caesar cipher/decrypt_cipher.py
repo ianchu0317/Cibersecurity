d1692aed23128eb4ca7b3f41e22f889853a0eab4
ianchu0317 <71509578+ianchu0317@users.noreply.github.com>
2021-01-14 18:27:23 -0300

    Add files via upload

---
+++ python hacking/caesar cipher/decrypt_cipher.py
@@ -0,0 +1,14 @@
+cipher = input("[+]Enter your string: ").strip()
+
+
+decrypt = ""
+a = "abcdefghijklmnopqrstuvwxyz".strip()
+
+for key in range(26+1):
+	decrypt = ""
+	for letter in cipher:
+		if letter in a:
+			decrypt += a[((a.find(letter))-key)]		
+		else:
+			break 
+	print(f"{key}:{decrypt}")
