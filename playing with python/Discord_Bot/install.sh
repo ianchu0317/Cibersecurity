24161592986043b7c7d4b61380683c9eedf56b3e
ianchu0317 <71509578+ianchu0317@users.noreply.github.com>
2021-01-31 11:38:32 -0300

    Update install.sh

--- playing with python/Discord_Bot/install.sh
+++ playing with python/Discord_Bot/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mv AI.py discord_bot.py random_comment.py search_image.py /usr/local/bin && chmod +x discord_bot.py
-
+mv AI.py discord_bot.py random_comment.py search_image.py /usr/local/bin && chmod +x /usr/local/bin/discord_bot.py
+pip install discord
 
